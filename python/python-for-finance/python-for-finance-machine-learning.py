6c2947847e8ac6659be0b42e765aecc360f2fc45
Ck <ckwong1204@gmail.com>
2018-10-08 22:20:01 +0800

    python-for-finance machine learning

---
+++ python/python-for-finance/python-for-finance-machine-learning.py
@@ -0,0 +1,109 @@
+# Machine learning - Python Programming for Finance p. 12
+# https://www.youtube.com/watch?v=W4kqEvGI4Lg
+
+import numpy as np
+import pandas as pd
+import pickle
+from collections import Counter
+
+def process_data_for_labels(ticker):
+    hm_days = 7
+    df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+    tickers = df.columns.values.tolist()
+
+    df.fillna(0, inplace=True)
+
+    for i in range(1, hm_days+1):
+        df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+
+    df.fillna(0, inplace=True)
+
+    return tickers, df
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+    return 0
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)] ))
+
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X,y,df
+
+
+
+from sklearn import svm, cross_validation, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+# from sklearn import svm, model_selection as cross_validation, neighbors﻿
+
+def do_ml(ticker):
+
+    X, y, df = extract_featuresets(ticker)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25)
+
+    # clf = neighbors.KNeighborsClassifier()
+
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
+
+    clf.fit(X_train, y_train)
+
+    confidence = clf.score(X_test, y_test)
+
+    print('accuracy:', confidence)
+    predictions = clf.predict(X_test)
+    print('predicted class counts:', Counter(predictions))
+    print()
+    print()
+
+    return confidence
+
+# do_ml('AMD')
+# do_ml('AAPL')
+# do_ml('ABT')
+
+from statistics import mean
+
+with open("sp500tickers.pickle","rb") as f:
+    tickers = pickle.load(f)
+
+accuracies = []
+for count,ticker in enumerate(tickers):
+
+    if count%10==0:
+        print(count)
+
+    accuracy = do_ml(ticker)
+    accuracies.append(accuracy)
+    print("{} accuracy: {}. Average accuracy:{}".format(ticker,accuracy,mean(accuracies)))
