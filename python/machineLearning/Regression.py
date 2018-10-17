6d371134b2db99c706056c9f7f7a47ff171d8168
Ck <ckwong1204@gmail.com>
2018-10-17 16:34:40 +0800

    pythonprogramming - Regression p6 - forecasting-predicting-machine-learning-tutorial

--- python/machineLearning/Regression.py
+++ python/machineLearning/Regression.py
@@ -1,12 +1,17 @@
 # https://pythonprogramming.net/regression-introduction-machine-learning-tutorial/?completed=/machine-learning-tutorial-python-introduction/
 
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
 
-df = quandl.get("WIKI/GOOGL")
+style.use('ggplot')
+
+df = quandl.get("WIKI/AMZN")
 
 print(df.head())
 
@@ -24,28 +29,60 @@ forecast_out = 20
 print("forecast_out: ", forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
 
-
+# accuracy
 # algorithm  https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
 # clf = svm.SVR()
 # clf = LinearRegression()
 clf = LinearRegression(n_jobs=-1) #-1 means using all processors
 clf.fit(X_train, y_train)
+with open('linearregression.pickle','wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 confidence = clf.score(X_test, y_test)
-print("LinearRegression:",confidence)
+print("LinearRegression:", confidence)
 
-for k in ['linear','poly','rbf','sigmoid']:
-    clf = svm.SVR(kernel=k)
-    clf.fit(X_train, y_train)
-    confidence = clf.score(X_test, y_test)
-    print(k,confidence)
+# accuracy of the other regression model
+## for k in ['linear','poly','rbf','sigmoid']:
+##     clf = svm.SVR(kernel=k)
+##     clf.fit(X_train, y_train)
+##     confidence = clf.score(X_test, y_test)
+##     print(k,confidence)
 
+forecast_set = clf.predict(X_lately)
+print(forecast_set, confidence, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+# plot lost 50 date data
+df.iloc[-50:]['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
