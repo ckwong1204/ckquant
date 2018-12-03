3fc13f65e0ebe923e96a0a05629e6f5403cd922e
C K WONG <c.k.wong@oocl.com>
2018-12-03 18:50:46 +0800

    add install.sh

---
+++ install/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo please copy the latest FutuOpenD_1.07_Ubuntu16.04.tar.gz to ~/
+
+cd ~
+tar -xzf ~/FutuOpenD_1.07_Ubuntu16.04.tar.gz
+tar -xzf ~/ckquant/install/ta-lib-0.4.0-src.tar.gz
