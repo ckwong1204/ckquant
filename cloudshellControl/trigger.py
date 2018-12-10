89d6f5c785baef864be1726ba8c0afb12875005d
C K WONG <c.k.wong@oocl.com>
2018-12-11 00:18:31 +0800

    fix import webdriver

--- cloudshellControl/trigger.py
+++ cloudshellControl/trigger.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import calendar
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
